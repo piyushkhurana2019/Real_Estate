7c536f28f55267a54ca803314d4b4e5a316f66f9
piyushkhurana2019 <piyushkhurana159@gmail.com>
2024-05-13 04:11:14 +0530

    Made the Profile Page Private

--- api/controllers.js/auth.controller.js
+++ api/controllers.js/auth.controller.js
@@ -1,51 +1,50 @@
-import User from "../models/user.model.js";
+import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
-import jwt from "jsonwebtoken";
-import { errorHandler } from '../utils/error.js'
+import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
-export const signup = async (req, res, next)=>{
-    const {username, email, password} = req.body;
-    const hashedPassword = bcryptjs.hashSync(password ,10);
-    const newUser = new User({username, email, password: hashedPassword});
+export const signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
+  try {
+    await newUser.save();
+    res.status(201).json('User created successfully!');
+  } catch (error) {
+    next(error);
+  }
+};
 
-    try{
-        await newUser.save();  //This will take some time therefore we will await here
-        res.status(201).json("User Created Successfully");
-    }catch(error){
-        next(error);
-    }
-
-}
 export const signin = async (req, res, next) => {
-    const { email, password } = req.body;
-    try {
-      const validUser = await User.findOne({ email });
-      if (!validUser) return next(errorHandler(404, 'User not found!'));
-      const validPassword = bcryptjs.compareSync(password, validUser.password);
-      if (!validPassword) return next(errorHandler(401, 'Wrong credentials!'));
-      const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
-      const { password: pass, ...rest } = validUser._doc;   //for showing everything to user except password
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, 'User not found!'));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, 'Wrong credentials!'));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .cookie('access_token', token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const google = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      const { password: pass, ...rest } = user._doc;
       res
         .cookie('access_token', token, { httpOnly: true })
         .status(200)
         .json(rest);
-    } catch (error) {
-      next(error);
-    }
-  };
-  export const google  = async (req, res, next) => {
-    try{
-      const user = await User.findOne({email: req.body.email})
-      if(user){
-        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
-      const { password: pass, ...rest } = validUser._doc; 
-      res
-        .cookie('access_token', token, { httpOnly: true })
-        .status(200)
-        .json(rest);
-      }
-      else{
-        const generatedPassword =
+    } else {
+      const generatedPassword =
         Math.random().toString(36).slice(-8) +
         Math.random().toString(36).slice(-8);
       const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
@@ -63,9 +62,18 @@ export const signin = async (req, res, next) => {
       res
         .cookie('access_token', token, { httpOnly: true })
         .status(200)
-        .json(rest); 
-      }
-    }catch(err){
-
+        .json(rest);
     }
+  } catch (error) {
+    next(error);
   }
+};
+
+// export const signOut = async (req, res, next) => {
+//   try {
+//     res.clearCookie('access_token');
+//     res.status(200).json('User has been logged out!');
+//   } catch (error) {
+//     next(error);
+//   }
+// };
