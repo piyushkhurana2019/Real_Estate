a2808fd238258925480d2ad12e523d660abd8ed0
piyushkhurana2019 <piyushkhurana159@gmail.com>
2024-05-12 12:28:22 +0530

    Sign Up API Route Created

--- api/index.js
+++ api/index.js
@@ -1,10 +1,13 @@
 import express from 'express'
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 
 dotenv.config();
-const app = express();
 
+const app = express();
+app.use(express.json());
 
 
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -16,3 +19,6 @@ app.listen(3000, ()=>{
     console.log("Server is listnening on por̉̉t 3000!");
 }
 );
+
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
