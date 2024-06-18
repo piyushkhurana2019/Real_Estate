cc66e1773ae8f461ed31e483c0df82ba695ac899
piyushkhurana2019 <piyushkhurana159@gmail.com>
2024-06-19 04:25:02 +0530

    Sign Out Functionality Added

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -10,7 +10,7 @@ import {
   deleteUserFailure,
   deleteUserStart,
   deleteUserSuccess,
-  // signOutUserStart,
+  signOutUserStart,
 } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
 
@@ -104,6 +104,21 @@ export default function Profile() {
     }
   };
 
+  const handleSignOut = async () => {
+    try {
+      dispatch(signOutUserStart());
+      const res = await fetch('/api/auth/signout');
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    } catch (error) {
+      dispatch(deleteUserFailure(data.message));
+    }
+  };
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -173,7 +188,7 @@ export default function Profile() {
           Delete account
         </span>
         <span
-        //  onClick={handleSignOut}
+         onClick={handleSignOut}
          className='text-red-700 cursor-pointer'>
           Sign out
         </span>
