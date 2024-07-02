dfa8321644500fdaa0bf42ff6e68d1189d37cc5a
piyushkhurana2019 <piyushkhurana159@gmail.com>
2024-07-03 00:57:37 +0530

    Update Header Search Form

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,35 +1,52 @@
-import React from 'react'
-import { FaSearch } from 'react-icons/fa'
-import { Link } from 'react-router-dom'
-import {useSelector} from 'react-redux'
+import { FaSearch } from 'react-icons/fa';
+import { Link, useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
 
 export default function Header() {
-  const { currentUser } = useSelector(state => state.user)
+  const { currentUser } = useSelector((state) => state.user);
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);  // To save rest of the search parameters if one of gets updated
+    urlParams.set('searchTerm', searchTerm);  // To set the searchTerm
+    const searchQuery = urlParams.toString(); //some of the search query are numbers so converting them into strings
+    navigate(`/search?${searchQuery}`);
+  };
+
+  useEffect(() => {   // To set the text in the search box also if text in the param gets changed
+    const urlParams = new URLSearchParams(location.search); // To get the url search parameters
+    const searchTermFromUrl = urlParams.get('searchTerm');  // Now this time we get the search term last time we set it
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
   return (
     <header className='bg-slate-200 shadow-md'>
-    <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
-      <Link to='/'>
-        <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-          <span className='text-slate-500'>AK </span>
-          <span className='text-slate-700'>Estate</span>
-        </h1>
-      </Link>
-      <form
-        // onSubmit={handleSubmit}
-        className='bg-slate-100 p-3 rounded-lg flex items-center'
-      >
-        <input
-          type='text'
-          placeholder='Search...'
-          className='bg-transparent focus:outline-none w-24 sm:w-64'
-        //   value={searchTerm}
-        //   onChange={(e) => setSearchTerm(e.target.value)}
-        />
-        <button>
-          <FaSearch className='text-slate-600' />
-        </button>
-      </form>
-      <ul className='flex gap-4'>
+      <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+        <Link to='/'>
+          <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+            <span className='text-slate-500'>AK</span>
+            <span className='text-slate-700'>Estate</span>
+          </h1>
+        </Link>
+        <form
+          onSubmit={handleSubmit}
+          className='bg-slate-100 p-3 rounded-lg flex items-center'
+        >
+          <input
+            type='text'
+            placeholder='Search...'
+            className='bg-transparent focus:outline-none w-24 sm:w-64'
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <button>
+            <FaSearch className='text-slate-600' />
+          </button>
+        </form>
+        <ul className='flex gap-4'>
           <Link to='/'>
             <li className='hidden sm:inline text-slate-700 hover:underline'>
               Home
@@ -49,10 +66,10 @@ export default function Header() {
               />
             ) : (
               <li className=' text-slate-700 hover:underline'> Sign in</li>
-             )} 
+            )}
           </Link>
         </ul>
       </div>
     </header>
-  )
+  );
 }
