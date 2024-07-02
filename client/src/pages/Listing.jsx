1b32e580813fa95990c512d309fe27da451f807d
piyushkhurana2019 <piyushkhurana159@gmail.com>
2024-07-03 00:07:27 +0530

    Search API Route Added

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -62,7 +62,7 @@ export default function Listing() {
             {listing.imageUrls.map((url) => (
               <SwiperSlide key={url}>
                 <div
-                  className='h-[550px]'
+                  className='h-[370px]'
                   style={{
                     background: `url(${url}) center no-repeat`,
                     backgroundSize: 'cover',
@@ -138,13 +138,13 @@ export default function Listing() {
             </ul>
             {currentUser && listing.userRef !== currentUser._id && !contact && (
               <button
-                onClick={() => setContact(true)}
+                // onClick={() => setContact(true)}
                 className='bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3'
               >
                 Contact landlord
               </button>
             )}
-            {contact && <Contact listing={listing} />}
+            {/* {contact && <Contact listing={listing} />} */}
           </div>
         </div>
       )}
